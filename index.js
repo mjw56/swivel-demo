5ceb2e8c40bed9899591b2fbf8d095e929fb455c
Mike Wilcox <mwilcox56@gmail.com>
2015-11-11 21:11:03 -0500

    sw registration

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var foo = 'bar';
+import swivel from 'swivel';
+
+if (('serviceWorker' in navigator)) {
+  navigator.serviceWorker
+    .register('/bundle.js')
+    .then(navigator.serviceWorker.ready)
+    .then(function (thing) {
+      console.log('registered', thing);
+      swivel.on('data', function handler (context, ...data) {
+        console.log('allo!');
+      });
+    });
+}
